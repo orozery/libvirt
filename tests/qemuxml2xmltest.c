ddd0a92e711d38c1af2eefd4f0c3de2bd9b995e8
Or Ozeri <oro@il.ibm.com>
2022-02-20 11:42:48 +0200

    qemu: add librbd encryption engine
    rbd encryption is new in qemu 6.1.0.
    This commit adds a new encryption engine property which
    allows the user to use this new encryption engine.
    
    Signed-off-by: Or Ozeri <oro@il.ibm.com>

--- tests/qemuxml2xmltest.c
+++ tests/qemuxml2xmltest.c
@@ -326,6 +326,7 @@ mymain(void)
     DO_TEST("disk-network-iscsi", QEMU_CAPS_VIRTIO_SCSI);
     DO_TEST("disk-network-gluster", NONE);
     DO_TEST("disk-network-rbd", NONE);
+    DO_TEST_CAPS_VER("disk-network-rbd-encryption", "4.2.1");
     DO_TEST("disk-network-source-auth", NONE);
     DO_TEST("disk-network-sheepdog", NONE);
     DO_TEST("disk-network-vxhs", NONE);
